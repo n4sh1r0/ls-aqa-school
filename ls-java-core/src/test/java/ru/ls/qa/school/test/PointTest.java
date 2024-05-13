68dded76abb89a05218651330f805dcea429d692
n4sh1r0 <measal@mail.ru>
2024-05-13 17:23:09 +0300

    Дописал 2 теста

--- ls-java-core/src/test/java/ru/ls/qa/school/test/PointTest.java
+++ ls-java-core/src/test/java/ru/ls/qa/school/test/PointTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PointTest {
     @Test
-    public void testDistanceBeetwenDifferentPoints() {
+    public void testDistanceBetweenDifferentPoints() {
 
         Point point1 = new Point(1, 1);
         Point point2 = new Point(0, 0);
@@ -18,16 +18,44 @@ public class PointTest {
         assertEquals(expectedDistance, actualDistance, 0.0001);
     }
     @Test
-    public void testDistanceBeetwenSamePoints() {
+    public void testDistanceBetweenSamePoints() {
 
         Point point1 = new Point(0, 0);
         Point point2 = new Point(0, 0);
 
         boolean samePoints;
+        int point1X = point1.getX();
+        int point2X = point2.getX();
+        int point1Y = point2.getY();
+        int point2Y = point2.getY();
 
-        samePoints = (point1.x == point2.x) && (point1.y == point2.y);
+
+        samePoints = (point1X == point2X) && (point1Y == point2Y);
 
         assertTrue(samePoints);
     }
+    @Test
+    public void testDistanceBetweenPoint2ToPoint1() {
+
+        Point point1 = new Point(1, 1);
+        Point point2 = new Point(4, 4);
+
+        double actualDistance = point2.distance(point1);
+        double expectedDistance = 4.242640687119285;
+
+        assertEquals(expectedDistance, actualDistance, 0.0001);
+    }
+
+    @Test
+    public void testDistanceBetweenOnePoint() {
+
+        Point point1 = new Point(1, 1);
+
+        double actualDistance = point1.distance(point1);
+        double expectedDistance = 0;
+
+        assertEquals(expectedDistance, actualDistance, 0.0001);
+    }
+
 }
 
