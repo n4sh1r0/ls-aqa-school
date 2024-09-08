b23464ed953db740e91599f1587f579cedd9b8d5
ASSenokosov <measal@mail.ru>
2024-09-08 23:13:59 +0300

    fix all tests

--- addressbook-web-tests/src/test/java/ru/ls/qa/school/addressbook/tests/GroupDeleteTest.java
+++ addressbook-web-tests/src/test/java/ru/ls/qa/school/addressbook/tests/GroupDeleteTest.java
@@ -7,23 +7,6 @@ import java.util.Random;
 
 public class GroupDeleteTest extends TestBase {
 
-    public static String generateRandomString(int length) {
-        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        Random random = new Random();
-        StringBuilder sb = new StringBuilder(length);
-
-        for (int i = 0; i < length; i++) {
-            sb.append(characters.charAt(random.nextInt(characters.length())));
-        }
-
-        return sb.toString();
-    }
-
-    public static int getRandomLength() {
-        Random random = new Random();
-        return 5 + random.nextInt(6);
-    }
-
     @Test
     public void testDeleteGroup() {
         page.getGroupListPage()
